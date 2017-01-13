fade7b6daff35a22f4673bb7ca0de4685698026a
Joshua Kairu <joshuawk@yandex.com>
2017-01-13 17:04:27 +0300

    refactor: Put stub code for large screens (which will need two-pane UIs)

--- app/src/main/java/com/joslittho/baker_order/activity/MainActivity.java
+++ app/src/main/java/com/joslittho/baker_order/activity/MainActivity.java
@@ -4,12 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.joslittho.baker_order.R;
+import com.joslittho.baker_order.R2;
+
+import butterknife.BindBool;
 
 public class MainActivity extends AppCompatActivity {
 
     /* CONSTANTS */
 
     /* VARIABLES */
+
+    /* Primitives */
+
+    @BindBool( R2.bool.is_large_screen )
+    public boolean mIsLargeScreen; // ditto
 
     /* METHODS */
 
@@ -23,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
         // 0. super stuff
         // 1. use the main activity layout
+        // 2. if we are on a large screen and thus need to use a two-pane UI
 
         // 0. super stuff
 
@@ -31,6 +40,15 @@ public class MainActivity extends AppCompatActivity {
         // 1. use the main activity layout
 
         setContentView( R.layout.activity_main );
+
+        // 2. if we are on a large screen and thus need to use a two-pane UI
+
+        // begin if we are on large screen
+        if ( mIsLargeScreen ) {
+
+
+
+        } // end if we are on large screen
 
     } // end onCreate
 
